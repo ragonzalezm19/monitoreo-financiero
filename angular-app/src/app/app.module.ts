a5865c8bab20ce3e10855af10ebcfea07314496a
Roberto Gonzalez <rgonzalez@creasys.cl>
2019-08-12 16:06:35 -0400

    Se agrega Bootstrap y manejo de layouts

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,12 +1,28 @@
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// Layouts
+import { MainLayoutComponent } from './layouts/main-layout.component';
+
+// Pages
+import { HomeComponent } from './pages/home/home.component';
+
+// Components
 import { AppComponent } from './app.component';
+import { MainNavbarComponent } from './components/main-navbar/main-navbar.component';
+import { MainFooterComponent } from './components/main-footer/main-footer.component';
+
+// Services
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MainLayoutComponent,
+    MainNavbarComponent,
+    MainFooterComponent,
   ],
   imports: [
     BrowserModule,
